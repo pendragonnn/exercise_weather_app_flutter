0fa059a2ec32e9089ab655bae06d0c60cb25bc74
Wisnu Andika <124800648+pendragonnn@users.noreply.github.com>
2024-12-13 22:36:18 +0700

    feat: implement geolocator & add home screen ui

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,23 +1,30 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:weather_app/app/modules/home/controllers/global_controller.dart';
+import 'package:weather_app/app/widgets/header_widget.dart';
 
-import '../controllers/home_controller.dart';
+// import '../controllers/home_controller.dart';
 
-class HomeView extends GetView<HomeController> {
+class HomeView extends GetView<GlobalController> {
   const HomeView({super.key});
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('HomeView'),
-        centerTitle: true,
-      ),
-      body: const Center(
-        child: Text(
-          'HomeView is working',
-          style: TextStyle(fontSize: 20),
-        ),
+    return SafeArea(
+      child: Scaffold(
+        body: Obx(() => (controller.checkLoading().isTrue)
+            ? const Center(
+                child: CircularProgressIndicator(),
+              )
+            : ListView(
+                scrollDirection: Axis.vertical,
+                children: [
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  const HeaderWidget(),
+                ],
+              )),
       ),
     );
   }
